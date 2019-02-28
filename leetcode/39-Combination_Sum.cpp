dfc638b9ce65bf242fb7b5c074fb1aae6f6a2f01
simon-lu <wq.lu@outlook.com>
2019-02-28 16:39:08 +0800

    add 39

--- leetcode/39-Combination_Sum.cpp
+++ leetcode/39-Combination_Sum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,12 +8,38 @@ class Solution {
 public:
     vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
         vector<vector<int> > ans;
-        findAns(ans, candidates, target);
+        vector<int> cur;
+        sort(candidates.begin(), candidates.end());
+        dfs(ans, cur, candidates, target);
         return ans;
     }
 
-    void findAns(vector<vector<int> >&ans, vector<int>& candidates, int target) {
-        
+    void dfs(vector<vector<int> >& ans, vector<int>& cur, vector<int>& candidates, int target) {
+        if (target == 0) {
+            ans.push_back(cur);
+        } else if (target < 0) {
+            return;
+        } else {
+            for (int candidate : candidates) {
+                cur.push_back(candidate);
+                dfs(ans, cur, candidates, target - candidate);
+                cur.pop_back();
+            }
+        }
     }
-
 };
+
+int main() {
+    Solution s;
+    vector<int> candidates;
+    candidates = {2, 3, 6, 7};
+    vector<vector<int> > ans = s.combinationSum(candidates, 7);
+    for (vector<int> v1 : ans) {
+        cout << "[ ";
+        for (int i : v1) {
+            cout << i << " "; 
+        }
+        cout << "]" << endl;
+    } 
+    return 0;
+}
