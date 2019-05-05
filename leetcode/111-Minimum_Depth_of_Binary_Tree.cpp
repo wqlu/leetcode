f7015a21797ae0fa6a0fd82ee30f978dc535cd31
simon-lu <wq.lu@outlook.com>
2019-05-05 15:11:14 +0800

    add 111

--- leetcode/111-Minimum_Depth_of_Binary_Tree.cpp
+++ leetcode/111-Minimum_Depth_of_Binary_Tree.cpp
@@ -1,30 +1,15 @@
-#include <iostream>
-
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class Solution {
 public:
     int minDepth(TreeNode* root) {
         if (!root) return 0;
-        int ans = 1;
-        ans = travelsal(root, ans);
-        return ans;
-    }
-
-    int travelsal(TreeNode*& root, int& ans) {
-        if (root->left || root->right) {
-            return ans;
-        } else {
-            ans += 1;
-            ans = min(travelsal(root->left, ans), travelsal(root->right, ans));
+        if (!root->left && !root->right) {
+            return 1;
         }
-        return ans;
+        if (!root->left)
+            return 1 + minDepth(root->right);
+        if (!root->right)
+            return 1 + minDepth(root->left);
+        
+        return 1 + min(minDepth(root->right), minDepth(root->left));
     }
 };
